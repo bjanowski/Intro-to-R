b5719a9393bf01ab666762b7abbe9fe7cce28edb
Thaddeus Janowski <tbj223@lehigh.edu>
2019-11-11 15:52:59 -0600

    Doing the assignment

---
+++ Assignment 05.R
@@ -0,0 +1,5 @@
+############################################################################
+#Thaddeus Janowski Assignment 05 - Unemployment Rate in McDonald County, MO#
+############################################################################
+DF <- read.csv("MOMCDO9URN.csv")
+data.table::fread("MOMCDO9URN.csv")
